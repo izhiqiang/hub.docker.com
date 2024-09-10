c30183d4da6ae658c781e7b0a5ca5f194cd9d1f0
zhiqiang <zhiqiang2033@gmail.com>
2024-09-10 16:44:52 +0800

    feat ubuntu-ssh

--- release.sh
+++ release.sh
@@ -11,3 +11,9 @@ docker login -u $DOCKER_USER -p $DOCKER_PWD
 echo "Release docker:openresty-alpine"
 docker build -f Dockerfile.openresty.alpine -t  zhiqiangwang/docker:openresty-alpine  .
 docker push zhiqiangwang/docker:openresty-alpine
+
+
+echo "Release docker:ubuntu-ssh"
+docker build -f Dockerfile.ssh -t  zhiqiangwang/docker:ubuntu-ssh  .
+docker push zhiqiangwang/docker:ubuntu-ssh
+
